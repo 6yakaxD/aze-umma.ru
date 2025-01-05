68369684069ec03eee7068b544c19478d46e49e9
rmtj <rm.tj.777@gmail.com>
2025-01-05 15:23:51 +0300

    navbar for mobile | burger menu

--- components/base/navbar.tsx
+++ components/base/navbar.tsx
@@ -1,13 +1,35 @@
-import React from 'react';
+"use client"
+
+import React, {useState} from 'react';
 import styles from './navbar.module.scss';
 import Link from "next/link";
 
 export default function Navbar(): React.ReactElement {
 
+    const [isOpen, setIsOpen] = useState(false);
+
+    const openNav = () => {
+        const nav = document.getElementById("myNav");
+        if (nav) {
+            nav.style.width = "100%";
+            setIsOpen(true);
+            document.body.classList.add(styles.noScroll);
+        }
+    };
+
+    const closeNav = () => {
+        const nav = document.getElementById("myNav");
+        if (nav) {
+            nav.style.width = "0%";
+            setIsOpen(false);
+            document.body.classList.remove(styles.noScroll);
+        }
+    };
+
     return (
         <>
-            <div className={styles.navbar} >
-                <a href="/" >ГЛАВНАЯ</a>
+            <div className={styles.navbar}>
+                <a href="/">ГЛАВНАЯ</a>
                 <a href="/about">ОБ ОРГАНИЗАЦИИ</a>
 
                 <div className={styles.dropdown}>
@@ -35,6 +57,25 @@ export default function Navbar(): React.ReactElement {
                 <a href="/contacts">КОНТАКТЫ</a>
                 <a href="/cooperation">СОТРУДНИЧЕСТВО</a>
                 <a href="/azerbaijan">АЗЕРБАЙДЖАН</a>
+            </div>
+
+
+            <div className={styles.overlayWrapper}>
+                <div id="myNav" className={styles.overlay}>
+                    <a className={styles.closebtn} onClick={closeNav}>&times;</a>
+                    <div className={styles.overlayContent}>
+                        <a href="/">ГЛАВНАЯ</a>
+                        <a href="/about">ОБ ОРГАНИЗАЦИИ</a>
+                        <a href="/structure">СТРУКТУРА</a>
+                        <a href="/news">НОВОСТИ</a>
+                        <a href="/contacts">КОНТАКТЫ</a>
+                        <a href="/cooperation">СОТРУДНИЧЕСТВО</a>
+                        <a href="/azerbaijan">АЗЕРБАЙДЖАН</a>
+                    </div>
+                </div>
+
+                <span className={`${isOpen ? styles.invertedBtn : styles.openNavButton}`}
+                      onClick={openNav}>&#9776;</span>
             </div>
         </>
     );
